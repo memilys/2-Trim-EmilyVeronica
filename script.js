a6764a34235c017f329dd256df1274d98aebc1a0
memilys <113731202+memilys@users.noreply.github.com>
2024-06-13 12:29:50 -0300

    13/06

--- script.js
+++ script.js
@@ -49,4 +49,16 @@ let perguntaAtual;
 function mostraPergunta() {
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
 }
+function mostraAlternativas() {
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativa.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+
+(const alternativa of perguntaAtual.alternativas)
